3cbb7f7bf3e542cd9d28b44661049cf77d6bcdc9
Ivan Gandacov <gandacov96@gmail.com>
2020-06-26 22:29:59 +0200

    Process script tags and count/display sorted number of occurrences

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,30 @@
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.GoogleSearchResult;
+import model.GoogleSearchResultItem;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
+import java.net.SocketTimeoutException;
 import java.net.URL;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
 public class App {
+
+    public static String getLibraryName(String scriptSrcAttr) {
+        String res = scriptSrcAttr.substring(scriptSrcAttr.lastIndexOf('/') + 1);
+
+        int dotIndex = res.indexOf('.');
+        if (dotIndex >= 0) {
+            res = res.substring(0, res.indexOf('.'));
+        }
+
+        return res;
+    }
 
     public static void main(String[] args) throws IOException {
 
@@ -23,12 +36,39 @@ public class App {
         ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         GoogleSearchResult searchResult = objectMapper.readValue(new File("mockGoogleResult.txt"), GoogleSearchResult.class);
 
-        Document doc = Jsoup.connect(searchResult.getItems().get(1).getLink()).get();
+        String url = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
+        String res = url.substring(url.lastIndexOf('/') + 1);
+        System.out.println(res.substring(0, res.indexOf('.')));
 
-        List<String> srcAttributes = doc.select("script").stream().map(scriptTag -> scriptTag.attr("src")).
-                filter(srcAttribute -> !srcAttribute.isEmpty()).collect(Collectors.toList());
+        HashMap<String, Integer> libraryToCount = new HashMap<>();
 
-        System.out.println(srcAttributes);
+        for (GoogleSearchResultItem item : searchResult.getItems()) {
+            System.out.println("Reading: " + item.getLink());
+            try {
+                Document doc = Jsoup.connect(item.getLink()).get();
+
+                List<String> srcAttributes = doc.select("script").stream().map(scriptTag -> scriptTag.attr("src")).
+                        filter(srcAttribute -> !srcAttribute.isEmpty()).collect(Collectors.toList());
+
+                srcAttributes.forEach(attr -> {
+                    String srcAttr = getLibraryName(attr);
+                    libraryToCount.putIfAbsent(srcAttr, -1);
+                    int newCount = libraryToCount.get(srcAttr) + 1;
+                    libraryToCount.put(srcAttr, newCount);
+                });
+
+                System.out.println(srcAttributes);
+            } catch (SocketTimeoutException timeoutException) {
+                System.out.println("Timeout!");
+            }
+        }
+
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(libraryToCount.entrySet());
+
+        Collections.sort(entries, Comparator.comparing(Map.Entry::getValue));
+
+        System.out.println(">>>>>>>>>>>>>>> RESULTS:");
+        entries.forEach(entry -> System.out.println(entry.getKey() + " -- " + entry.getValue()));
 
         reader.close();
     }
